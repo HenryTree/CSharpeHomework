fbbd0e46ad3519c77119ee3178426891b7ce5295
HenryTree <842456775@qq.com>
2019-02-26 12:29:00 +0800

    First Homewwork

---
+++ Homework1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = "";
+            string string2 = "";
+            int num1 = 0;
+            int num2 = 0;
+            double ans = 0;
+            Console.Write("Please input an int:");
+            string1 = Console.ReadLine();
+            num1 = Int32.Parse(string1);
+            Console.Write("Please input another int:");
+            string2 = Console.ReadLine();
+            num2 = Int32.Parse(string2);
+            ans = num1 * num2;
+            Console.Write("The answer is:");
+            Console.Write(ans);
+        }
+    }
+}
---
+++ Homework1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            string s2 = textBox2.Text;
+            double d1 = double.Parse(s1);
+            double d2 = double.Parse(s2);
+            double mul = d1 * d2;
+            this.textBox3.Text = "结果是：" + mul;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
